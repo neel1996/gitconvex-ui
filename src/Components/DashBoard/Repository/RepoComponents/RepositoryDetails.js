470ba69cb6d48a339693571421aa3cc57785b19f
Neel <neelusenthil1996@gmail.com>
2020-07-28 23:55:52 +0530

    Added folder navigation to repo details view
    - Enhancement to the file list in repo details section to include folder navigation

--- src/Components/DashBoard/Repository/RepoComponents/RepositoryDetails.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepositoryDetails.js
@@ -8,6 +8,7 @@ import { v4 as uuid } from "uuid";
 import {
   globalAPIEndpoint,
   ROUTE_REPO_DETAILS,
+  GIT_FOLDER_CONTENT,
 } from "../../../../util/env_config";
 import AddBranchComponent from "./RepoDetailBackdrop/AddBranchComponent";
 import AddRemoteRepoComponent from "./RepoDetailBackdrop/AddRemoteRepoComponent";
@@ -27,6 +28,8 @@ export default function RepositoryDetails(props) {
   const [backdropToggle, setBackdropToggle] = useState(false);
   const [selectedBranch, setSelectedBranch] = useState("");
   const [currentBranch, setCurrentBranch] = useState("");
+  const [gitRepoFiles, setGitRepoFiles] = useState([]);
+  const [gitFileBasedCommits, setGitFileBasedCommits] = useState([]);
   const [action, setAction] = useState("");
 
   const closeBackdrop = (toggle) => {
@@ -123,20 +126,18 @@ export default function RepositoryDetails(props) {
       })
         .then((res) => {
           if (res.data && res.data.data && !res.data.error) {
-            let gitRemoteLocal =
-              res.data.data.gitConvexApi.gitRepoStatus.gitRemoteData;
-            setCurrentBranch(
-              res.data.data.gitConvexApi.gitRepoStatus.gitCurrentBranch
-            );
+            const localRepoStatus = res.data.data.gitConvexApi.gitRepoStatus;
+            let gitRemoteLocal = localRepoStatus.gitRemoteData;
+            setCurrentBranch(localRepoStatus.gitCurrentBranch);
             if (gitRemoteLocal.includes("||")) {
               setIsMultiRemote(true);
-              res.data.data.gitConvexApi.gitRepoStatus.gitRemoteData = gitRemoteLocal.split(
-                "||"
-              )[0];
+              localRepoStatus.gitRemoteData = gitRemoteLocal.split("||")[0];
               setIsMultiRemote(true);
               setMultiRemoteCount(gitRemoteLocal.split("||").length);
             }
-            setGitRepoStatus(res.data.data.gitConvexApi.gitRepoStatus);
+            setGitRepoStatus(localRepoStatus);
+            setGitRepoFiles([...localRepoStatus.gitTrackedFiles]);
+            setGitFileBasedCommits([...localRepoStatus.gitFileBasedCommit]);
           } else {
             setRepoFetchFailed(true);
           }
@@ -150,7 +151,7 @@ export default function RepositoryDetails(props) {
     }
   }, [props.parentProps, backdropToggle]);
 
-  const {
+  let {
     gitRemoteData,
     gitRepoName,
     gitBranchList,
@@ -158,8 +159,6 @@ export default function RepositoryDetails(props) {
     gitRemoteHost,
     gitTotalCommits,
     gitLatestCommit,
-    gitTrackedFiles,
-    gitFileBasedCommit,
   } = gitRepoStatus;
 
   const switchBranchHandler = (branchName) => {
@@ -447,38 +446,105 @@ export default function RepositoryDetails(props) {
   };
 
   const gitTrackedFileComponent = () => {
+    const fetchFolderContent = (directoryName) => {
+      if (repoIdState && directoryName) {
+        setGitRepoFiles([]);
+        setGitFileBasedCommits([]);
+
+        const payload = JSON.stringify(
+          JSON.stringify({ repoId: repoIdState, directoryName })
+        );
+
+        axios({
+          url: globalAPIEndpoint,
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          data: {
+            query: `
+
+            query GitConvexApi
+            {
+              gitConvexApi(route: "${GIT_FOLDER_CONTENT}", payload: ${payload}){
+                gitFolderContent{
+                  gitTrackedFiles
+                  gitFileBasedCommit
+                }
+              }
+            }
+          `,
+          },
+        })
+          .then((res) => {
+            if (res.data.data && !res.data.error) {
+              const localFolderContent =
+                res.data.data.gitConvexApi.gitFolderContent;
+              setGitRepoFiles([...localFolderContent.gitTrackedFiles]);
+              setGitFileBasedCommits([
+                ...localFolderContent.gitFileBasedCommit,
+              ]);
+            } else {
+              console.log(
+                "ERROR: Error occurred while fetching the folder content!"
+              );
+            }
+          })
+          .catch((err) => {
+            if (err) {
+              console.log("API GitStatus error occurred : " + err);
+            }
+          });
+      }
+    };
+
     if (
-      gitTrackedFiles &&
-      gitTrackedFiles.length > 0 &&
-      gitTrackedFiles[0] !== "NO_TRACKED_FILES"
+      gitRepoFiles &&
+      gitRepoFiles.length > 0 &&
+      gitRepoFiles[0] !== "NO_TRACKED_FILES"
     ) {
       var formattedFiles = [];
       var directoryEntry = [];
       var fileEntry = [];
 
-      gitTrackedFiles.forEach((entry, index) => {
+      gitRepoFiles.forEach((entry, index) => {
         const splitEntry = entry.split(":");
 
         if (splitEntry[1].includes("directory")) {
+          let directorypath = splitEntry[0];
+          if (directorypath.includes("/")) {
+            directorypath = directorypath.split("/")[
+              directorypath.split("/").length - 1
+            ];
+          } else if (directorypath.includes("\\")) {
+            directorypath = directorypath.split("\\")[
+              directorypath.split("\\").length - 1
+            ];
+          }
           directoryEntry.push(
             <div
               className="block w-full p-2 border-b border-gray-300"
               key={`directory-key-${uuid()}`}
             >
-              <div className="flex">
+              <div className="flex cursor-pointer">
                 <div className="w-1/6">
                   <FontAwesomeIcon
                     icon={["fas", "folder"]}
                     className="font-sans text-xl text-blue-600"
                   ></FontAwesomeIcon>
                 </div>
-                <div className="w-2/4 text-gray-800 text-lg mx-3 font-sans">
-                  {splitEntry[0]}
+                <div
+                  className="w-2/4 text-gray-800 text-lg mx-3 font-sans hover:text-indigo-400 hover:font-semibold"
+                  onClick={(event) => {
+                    fetchFolderContent(splitEntry[0]);
+                  }}
+                >
+                  {directorypath}
                 </div>
 
-                <div className="w-2/4 p-2 bg-green-200 text-green-900 truncate ... rounded-lg text-left mx-auto w-3/5">
-                  {gitFileBasedCommit[index]
-                    ? gitFileBasedCommit[index]
+                <div className="w-2/4 p-2 bg-green-200 text-green-900 truncate rounded-lg text-left mx-auto w-3/5">
+                  {gitFileBasedCommits[index]
+                    ? gitFileBasedCommits[index]
                         .split(" ")
                         .filter((entry, index) => {
                           return index !== 0 ? entry : null;
@@ -506,8 +572,8 @@ export default function RepositoryDetails(props) {
                   {splitEntry[0]}
                 </div>
                 <div className="w-2/4 p-2 bg-indigo-200 truncate ... text-indigo-900 rounded-lg text-left mx-auto w-3/5">
-                  {gitFileBasedCommit[index]
-                    ? gitFileBasedCommit[index]
+                  {gitFileBasedCommits[index]
+                    ? gitFileBasedCommits[index]
                         .split(" ")
                         .filter((entry, index) => {
                           return index !== 0 ? entry : null;
@@ -526,7 +592,7 @@ export default function RepositoryDetails(props) {
 
       return (
         <div
-          className="block mx-auto justify-center p-2 text-blue-600 cursor-pointer hover:text-blue-700"
+          className="block mx-auto justify-center p-2 text-blue-600 hover:text-blue-700"
           key="repo-key"
         >
           <div className="flex justify-around w-full p-2 mx-auto pb-6 border-b border-blue-400">
@@ -537,7 +603,7 @@ export default function RepositoryDetails(props) {
           {formattedFiles}
         </div>
       );
-    } else if (gitTrackedFiles && gitTrackedFiles[0] === "NO_TRACKED_FILES") {
+    } else if (gitRepoFiles && gitRepoFiles[0] === "NO_TRACKED_FILES") {
       return (
         <div className="bg-gray-400 rounded-lg text-black text-2xl text-center">
           No Tracked Files in the repo!
