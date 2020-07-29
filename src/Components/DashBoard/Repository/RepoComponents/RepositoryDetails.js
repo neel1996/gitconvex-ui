6fe8534b155f8912d987ca51eafbb4534cd9f5af
Neel <neelusenthil1996@gmail.com>
2020-07-29 23:28:24 +0530

    Added folder navigation feature to repo view and updated index.css file - Feature delivery for folder navigation from the repo details view - Generated new index.css file with tailwind v1.6.0 - Removed tailwind config file as it will not be required for tailwind v1.6.0+

--- src/Components/DashBoard/Repository/RepoComponents/RepositoryDetails.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepositoryDetails.js
@@ -30,6 +30,7 @@ export default function RepositoryDetails(props) {
   const [currentBranch, setCurrentBranch] = useState("");
   const [gitRepoFiles, setGitRepoFiles] = useState([]);
   const [gitFileBasedCommits, setGitFileBasedCommits] = useState([]);
+  const [directoryNavigator, setDirectoryNavigator] = useState([]);
   const [action, setAction] = useState("");
 
   const closeBackdrop = (toggle) => {
@@ -82,6 +83,29 @@ export default function RepositoryDetails(props) {
       <AddRemoteRepoComponent repoId={repoIdState}></AddRemoteRepoComponent>
     );
   }, [repoIdState]);
+
+  function filterNullCommitEntries(gitTrackedFiles, gitFileBasedCommit) {
+    let localGitCommits = gitFileBasedCommit;
+    let localTrackedFiles = gitTrackedFiles.filter((item, index) => {
+      if (item) {
+        return true;
+      } else {
+        localGitCommits[index] = "";
+        return false;
+      }
+    });
+
+    localGitCommits = localGitCommits.filter((commit) => {
+      if (commit) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    setGitRepoFiles([...localTrackedFiles]);
+    setGitFileBasedCommits([...localGitCommits]);
+  }
 
   useEffect(() => {
     const endpointURL = globalAPIEndpoint;
@@ -136,8 +160,10 @@ export default function RepositoryDetails(props) {
               setMultiRemoteCount(gitRemoteLocal.split("||").length);
             }
             setGitRepoStatus(localRepoStatus);
-            setGitRepoFiles([...localRepoStatus.gitTrackedFiles]);
-            setGitFileBasedCommits([...localRepoStatus.gitFileBasedCommit]);
+            filterNullCommitEntries(
+              localRepoStatus.gitTrackedFiles,
+              localRepoStatus.gitFileBasedCommit
+            );
           } else {
             setRepoFetchFailed(true);
           }
@@ -445,59 +471,103 @@ export default function RepositoryDetails(props) {
     }
   };
 
-  const gitTrackedFileComponent = () => {
-    const fetchFolderContent = (directoryName) => {
-      if (repoIdState && directoryName) {
-        setGitRepoFiles([]);
-        setGitFileBasedCommits([]);
+  const fetchFolderContent = (
+    directoryName,
+    slicePosition,
+    sliceIndicator,
+    homeIndicator
+  ) => {
+    if (repoIdState) {
+      setGitRepoFiles([]);
+      setGitFileBasedCommits([]);
+      let localDirNavigator = directoryNavigator;
 
-        const payload = JSON.stringify(
-          JSON.stringify({ repoId: repoIdState, directoryName })
-        );
+      if (sliceIndicator) {
+        let slicedDirectory = localDirNavigator.slice(0, slicePosition);
+        if (slicedDirectory.length > 0) {
+          directoryName = slicedDirectory.join("/") + "/" + directoryName;
+        }
+      }
 
-        axios({
-          url: globalAPIEndpoint,
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          data: {
-            query: `
+      const payload = JSON.stringify(
+        JSON.stringify({ repoId: repoIdState, directoryName })
+      );
 
-            query GitConvexApi
-            {
-              gitConvexApi(route: "${GIT_FOLDER_CONTENT}", payload: ${payload}){
-                gitFolderContent{
-                  gitTrackedFiles
-                  gitFileBasedCommit
-                }
+      axios({
+        url: globalAPIEndpoint,
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        data: {
+          query: `
+
+          query GitConvexApi
+          {
+            gitConvexApi(route: "${GIT_FOLDER_CONTENT}", payload: ${payload}){
+              gitFolderContent{
+                gitTrackedFiles
+                gitFileBasedCommit
               }
             }
-          `,
-          },
-        })
-          .then((res) => {
-            if (res.data.data && !res.data.error) {
-              const localFolderContent =
-                res.data.data.gitConvexApi.gitFolderContent;
-              setGitRepoFiles([...localFolderContent.gitTrackedFiles]);
-              setGitFileBasedCommits([
-                ...localFolderContent.gitFileBasedCommit,
-              ]);
-            } else {
-              console.log(
-                "ERROR: Error occurred while fetching the folder content!"
-              );
-            }
-          })
-          .catch((err) => {
-            if (err) {
-              console.log("API GitStatus error occurred : " + err);
-            }
-          });
-      }
-    };
+          }
+        `,
+        },
+      })
+        .then((res) => {
+          if (res.data.data && !res.data.error) {
+            const localFolderContent =
+              res.data.data.gitConvexApi.gitFolderContent;
 
+            filterNullCommitEntries(
+              localFolderContent.gitTrackedFiles,
+              localFolderContent.gitFileBasedCommit
+            );
+
+            directoryName = directoryName.split("/")[
+              directoryName.split("/").length - 1
+            ];
+
+            if (homeIndicator) {
+              setDirectoryNavigator([]);
+              return;
+            }
+
+            if (directoryNavigator.length === 0) {
+              setDirectoryNavigator([directoryName]);
+            } else {
+              if (
+                sliceIndicator &&
+                slicePosition < directoryNavigator.length - 1
+              ) {
+                const iterator =
+                  directoryNavigator.length - (slicePosition + 1);
+
+                for (let i = 0; i < iterator; i++) {
+                  localDirNavigator.pop();
+                }
+                setDirectoryNavigator([...localDirNavigator]);
+              } else {
+                setDirectoryNavigator([...directoryNavigator, directoryName]);
+              }
+            }
+          } else {
+            console.log(
+              "ERROR: Error occurred while fetching the folder content!"
+            );
+          }
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(
+              "ERROR: Error occurred while fetching the folder content!"
+            );
+          }
+        });
+    }
+  };
+
+  const gitTrackedFileComponent = () => {
     if (
       gitRepoFiles &&
       gitRepoFiles.length > 0 &&
@@ -536,7 +606,7 @@ export default function RepositoryDetails(props) {
                 <div
                   className="w-2/4 text-gray-800 text-lg mx-3 font-sans hover:text-indigo-400 hover:font-semibold"
                   onClick={(event) => {
-                    fetchFolderContent(splitEntry[0]);
+                    fetchFolderContent(splitEntry[0], 0, false);
                   }}
                 >
                   {directorypath}
@@ -675,6 +745,50 @@ export default function RepositoryDetails(props) {
                 {gitRepoStatus ? gitRepoLeftPane() : null}
               </div>
             </div>
+            {directoryNavigator ? (
+              <div className="mx-6 p-3 font-sans flex gap-4 items-center justify-start">
+                <div
+                  className="w-1/6 text-gray-700 bg-gray-200 border border-dashed cursor-pointer justify-center p-3 text-center rounded shadow-md flex gap-4 my-auto items-center mx-4 text-xl"
+                  onClick={() => {
+                    fetchFolderContent("", 0, false, true);
+                  }}
+                >
+                  <div>
+                    <FontAwesomeIcon icon={["fas", "home"]}></FontAwesomeIcon>
+                  </div>
+                  <div>Home</div>
+                </div>
+                <div
+                  className="flex gap-4 items-center w-3/4 break-words overflow-x-auto"
+                  id="repoFolderNavigator"
+                >
+                  {directoryNavigator.map((item, index) => {
+                    return (
+                      <div
+                        className="flex gap-2 justify-start items-center"
+                        key={item + "-" + index}
+                      >
+                        <div
+                          className={`${
+                            index !== directoryNavigator.length - 1
+                              ? "text-blue-600 font-semibold hover:underline hover:text-blue-700 cursor-pointer"
+                              : ""
+                          } text-xl`}
+                          onClick={() => {
+                            if (index !== directoryNavigator.length - 1) {
+                              fetchFolderContent(item, index, true);
+                            }
+                          }}
+                        >
+                          {item}
+                        </div>
+                        <div>/</div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ) : null}
             <div className="block w-11/12 my-6 mx-auto justify-center p-6 rounded-lg bg-gray-100 p-2 shadow-md overflow-auto">
               {gitRepoStatus ? gitTrackedFileComponent() : null}
             </div>
