d4a950000cd441968cb204f2830bdc62d9310f13
Neel <neelusenthil1996@gmail.com>
2020-09-04 10:27:00 +0530

    added error view for invalid file types in code view component

--- src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CodeFileViewComponent.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CodeFileViewComponent.js
@@ -15,6 +15,7 @@ export default function CodeFileViewComponent(props) {
   const [prismIndicator, setPrismIndicator] = useState("");
   const [highlightedCode, setHighlightedCode] = useState([]);
   const [fileDataState, setFileDataState] = useState([]);
+  const [isInvalidFile, setIsInvalidFile] = useState(false);
 
   const repoId = props.repoId;
   const fileItem = props.fileItem;
@@ -79,6 +80,7 @@ export default function CodeFileViewComponent(props) {
       })
       .catch((err) => {
         console.log(err);
+        setIsInvalidFile(true);
       });
   }, [repoId, fileItem]);
 
@@ -104,34 +106,42 @@ export default function CodeFileViewComponent(props) {
 
   return (
     <div className="p-6 rounded-md shadow-sm flex justify-center mx-auto my-auto mb-6 w-full h-full">
-      <div className="mx-auto mb-10 w-11/12 h-full p-6 rounded shadow border-gray-100 bg-white">
-        <div className="flex w-11/12 mx-auto justify-between gap-4">
-          {languageState
-            ? topPanePills("Language", languageState, "pink")
-            : null}
-          {numberOfLines
-            ? topPanePills("Lines", numberOfLines, "orange")
-            : null}
+      {isInvalidFile ? (
+        <div className="w-full mx-auto my-auto p-6 rounded bg-red-200 text-red-600 font-sans text-2xl font-light text-center border-b-8 border-red-400 border-dashed">
+          File cannot be opened!
         </div>
-        {latestCommit ? (
-          <div className="block mx-auto my-6 w-11/12">
-            {topPanePills("Latest Commit", latestCommit, "indigo")}
+      ) : (
+        <div className="mx-auto mb-10 w-11/12 h-auto p-6 rounded shadow border-gray-100 bg-white">
+          <div className="flex w-11/12 mx-auto justify-between gap-4">
+            {languageState
+              ? topPanePills("Language", languageState, "pink")
+              : null}
+            {numberOfLines
+              ? topPanePills("Lines", numberOfLines, "orange")
+              : null}
           </div>
-        ) : null}
+          {latestCommit ? (
+            <div className="block mx-auto my-6 w-11/12">
+              {topPanePills("Latest Commit", latestCommit, "indigo")}
+            </div>
+          ) : null}
 
-        {fileDataState && prismIndicator ? (
-          <div
-            className="my-10 mx-auto p-4 rounded shadow overflow-auto"
-            style={{ height: "500px" }}
-          >
-            <pre>
-              <code
-                dangerouslySetInnerHTML={{ __html: highlightedCode.join("\n") }}
-              ></code>
-            </pre>
-          </div>
-        ) : null}
-      </div>
+          {fileDataState && prismIndicator ? (
+            <div
+              className="my-10 mx-auto p-4 rounded shadow overflow-auto"
+              style={{ height: "500px" }}
+            >
+              <pre>
+                <code
+                  dangerouslySetInnerHTML={{
+                    __html: highlightedCode.join("\n"),
+                  }}
+                ></code>
+              </pre>
+            </div>
+          ) : null}
+        </div>
+      )}
     </div>
   );
 }
