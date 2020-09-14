059d8642e98eea20c8888f0cb4ff3dea59ee214c
Neel <neelusenthil1996@gmail.com>
2020-09-15 00:03:01 +0530

    extracted styles for repo details commit logs component - part2

--- src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogComponent.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogComponent.js
@@ -381,37 +381,37 @@ export default function RepositoryCommitLogComponent(props) {
           return (
             <div
               id={`commitLogCard-${hash}`}
-              className="p-6 pb-6 rounded-lg shadow-sm block justify-center mx-auto my-4 bg-white w-full border-b-8 border-indigo-400"
+              className="repo-backdrop--commitlogs"
               key={hash}
             >
-              <div className="flex justify-between text-indigo-500">
-                <div className="text-2xl font-sans mx-auto">
+              <div className="commitlogs--toppane">
+                <div className="commitlogs--toppane--label">
                   <FontAwesomeIcon
                     icon={["fas", "calendar-alt"]}
                   ></FontAwesomeIcon>
-                  <span className="mx-2 border-b-2 border-dashed">
+                  <span className="commitlogs--toppane--data">
                     {formattedCommitTime}
                   </span>
                 </div>
-                <div className="p-1 h-auto border-r"></div>
-                <div className="text-2xl font-sans mx-auto">
+                <div className="commitlogs--toppane--divider"></div>
+                <div className="commitlogs--toppane--label">
                   <FontAwesomeIcon
                     icon={["fab", "slack-hash"]}
                   ></FontAwesomeIcon>
-                  <span className="mx-2 border-b-2 border-dashed">{hash}</span>
+                  <span className="commitlogs--toppane--data">{hash}</span>
                 </div>
-                <div className="p-1 h-auto border-r"></div>
-                <div className="text-2xl font-sans mx-auto">
+                <div className="commitlogs--toppane--divider"></div>
+                <div className="commitlogs--toppane--label">
                   <FontAwesomeIcon
                     icon={["fas", "user-ninja"]}
                   ></FontAwesomeIcon>
-                  <span className="mx-2 border-b-2 border-dashed truncate">
+                  <span className="commitlogs--toppane--data truncate">
                     {author}
                   </span>
                 </div>
               </div>
 
-              <div className="p-3 my-4 text-2xl font-semibold text-gray-600 font-sans flex justify-evenly items-center">
+              <div className="commitlogs--message">
                 <div className="w-1/8">
                   <FontAwesomeIcon
                     icon={["fas", "code"]}
@@ -421,17 +421,17 @@ export default function RepositoryCommitLogComponent(props) {
                 <div className="w-5/6 mx-3">{commitMessage}</div>
               </div>
 
-              <div className="w-11/12 flex justify-between mx-auto mt-4 text-xl text-gray-600">
-                <div className="w-1/3 flex justify-center my-auto items-center align-middle">
+              <div className="commitlogs--bottompane">
+                <div className="commitlogs--bottompane--label">
                   <div>
                     <FontAwesomeIcon icon={["far", "clock"]}></FontAwesomeIcon>
                   </div>
-                  <div className="mx-2 border-dashed border-b-4">
+                  <div className="commitlogs--bottompane--data">
                     {commitRelativeTime}
                   </div>
                 </div>
                 <div
-                  className="w-1/3 flex justify-around my-auto text-3xl font-sans font-light text-gray-600 items-center align-middle cursor-pointer pt-10"
+                  className="commitlogs--expand"
                   onClick={(event) => {
                     if (commitFilesCount) {
                       event.currentTarget.classList.add("hidden");
@@ -450,13 +450,13 @@ export default function RepositoryCommitLogComponent(props) {
                     ></FontAwesomeIcon>
                   )}
                 </div>
-                <div className="w-1/3 flex justify-center my-auto items-center align-middle">
+                <div className="commitlogs--bottompane--label">
                   <div>
                     <FontAwesomeIcon
                       icon={["far", "plus-square"]}
                     ></FontAwesomeIcon>
                   </div>
-                  <div className="mx-2 border-dashed border-b-4">
+                  <div className="commitlogs--bottompane--data">
                     {commitFilesCount ? (
                       `${commitFilesCount} Files`
                     ) : (
@@ -470,7 +470,7 @@ export default function RepositoryCommitLogComponent(props) {
         })}
       {excessCommit ? (
         <div
-          className="flex fixed bottom-0 right-0 w-16 h-16 mx-auto p-6 rounded-full shadow text-center bg-indigo-500 text-white text-2xl my-6 mr-6 cursor-pointer"
+          className="commitlogs--load-btn"
           title="Click to load commits"
           onClick={() => {
             if (totalCommitCount - skipLimit >= 10) {
@@ -484,7 +484,7 @@ export default function RepositoryCommitLogComponent(props) {
         </div>
       ) : null}
       {isLoading && totalCommitCount ? (
-        <div className="my-4 rounded p-3 bg-gray-100 text-lf font-semibold text-gray-800 text-center mx-auto">
+        <div className="commitlogs--loading">
           Loading {totalCommitCount - skipLimit} more commits...
           <div className="flex mx-auto my-6 text-center justify-center">
             <InfiniteLoader loadAnimation={isLoading}></InfiniteLoader>
--- src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogFileCard.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/CommitLogFileCard.js
@@ -54,10 +54,11 @@ export default function CommitLogFileCard({
         setIsLoading(false);
       });
   }, [repoId, commitHash]);
+
   return (
-    <div className="w-11/12 p-6 rounded-lg shadow-sm block justify-center mx-auto my-6 bg-blue-100">
+    <div className="commitlogs--files">
       <div
-        className="font-sans right-0 float-right font-light cursor-pointer mx-auto text-blue-400 text-2xl my-0"
+        className="commitlogs--files--close"
         style={{ marginTop: "-20px" }}
         onClick={() => {
           setCommitFiles([]);
@@ -67,14 +68,14 @@ export default function CommitLogFileCard({
         x
       </div>
       {isLoading ? (
-        <div className="mx-4 text-2xl text-gray-700 font-sans font-light text-center">
+        <div className="commitlogs--files--header text-center">
           Fetching changed files...
         </div>
       ) : null}
       {!isLoading && commitFiles ? (
-        <div className="mx-4 text-2xl text-gray-700 font-sans font-light">{`${commitFiles.length} Files changed`}</div>
+        <div className="commitlogs--files--header">{`${commitFiles.length} Files changed`}</div>
       ) : null}
-      <div className="block w-3/4 my-4 mx-10">
+      <div className="commitlogs--files--list">
         {commitFiles &&
           commitFiles.map(({ type, fileName }) => {
             let iconSelector = "";
@@ -100,7 +101,7 @@ export default function CommitLogFileCard({
 
             return (
               <div
-                className="flex justify-evenly items-center my-auto align-middle"
+                className="commitlogs--files--list--item"
                 key={fileName + commitHash}
               >
                 <div className={`w-1/4 text-2xl ${colorSelector}`}>
@@ -108,7 +109,7 @@ export default function CommitLogFileCard({
                     icon={["far", iconSelector]}
                   ></FontAwesomeIcon>
                 </div>
-                <div className="w-3/4 text-sm font-sans font-medium text-gray-700 truncate">
+                <div className="commitlogs--files--list--filename">
                   {fileName}
                 </div>
               </div>
