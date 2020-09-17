ed71692e8db5b9f8a4072a7e082556604f5b41a9
Neel <neelusenthil1996@gmail.com>
2020-09-18 00:51:18 +0530

    added search result card component to compare view

--- src/Components/DashBoard/Compare/RepoSearchBar.js
+++ src/Components/DashBoard/Compare/RepoSearchBar.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SearchRepoCards from "./SearchRepoCards";
 
 export default function RepoSearchBar() {
   library.add(fas);
+
+  const [toggleSearchResult, setToggleSearchResult] = useState(false);
+  const [searchQueryState, setSearchQueryState] = useState("");
+
   return (
-    <div className="compare--searchbar flex w-11/12 rounded-md justify-between shadow my-4 mx-auto items-center border">
-      <div className="w-11/12 rounded-r-md">
-        <input
-          type="text"
-          className="w-full p-3 outline-none text-lg font-light font-sans"
-          placeholder="Enter repo name to search"
-          onChange={(event) => {}}
-        />
+    <>
+      <div className="compare--searchbar flex w-11/12 rounded-md justify-between shadow mt-4 mx-auto items-center border">
+        <div className="w-11/12 rounded-r-md">
+          <input
+            type="text"
+            className="w-full p-3 outline-none text-lg font-light font-sans"
+            placeholder="Enter repo name to search"
+            value={searchQueryState}
+            onChange={(event) => {
+              setToggleSearchResult(true);
+              setSearchQueryState(event.currentTarget.value);
+            }}
+          />
+        </div>
+        <div
+          className="px-6 py-4 bg-gray-200 text-center rounded-r-lg hover:bg-gray-400 cursor-pointer"
+          onClick={() => {}}
+        >
+          <FontAwesomeIcon
+            icon={["fas", "search"]}
+            className="text-3xl text-gray-600"
+          ></FontAwesomeIcon>
+        </div>
       </div>
-      <div
-        className="px-6 py-4 bg-gray-200 text-center rounded-r-lg hover:bg-gray-400 cursor-pointer"
-        onClick={() => {}}
-      >
-        <FontAwesomeIcon
-          icon={["fas", "search"]}
-          className="text-3xl text-gray-600"
-        ></FontAwesomeIcon>
-      </div>
-    </div>
+      {toggleSearchResult && searchQueryState ? (
+        <div className="w-11/12 mx-auto rounded-b-md p-3 border">
+          <SearchRepoCards searchQuery={searchQueryState}></SearchRepoCards>
+        </div>
+      ) : null}
+    </>
   );
 }
--- src/Components/DashBoard/Compare/SearchRepoCards.js
+++ src/Components/DashBoard/Compare/SearchRepoCards.js
@@ -1,5 +1,82 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { globalAPIEndpoint, ROUTE_FETCH_REPO } from "../../../util/env_config";
 
-export default function SearchRepoCards() {
-  return <div></div>;
+export default function SearchRepoCards(props) {
+  const [repo, setRepo] = useState([]);
+
+  useEffect(() => {
+    const token = axios.CancelToken;
+    const source = token.source();
+
+    axios({
+      url: globalAPIEndpoint,
+      method: "POST",
+      cancelToken: source.token,
+      data: {
+        query: `
+          query GitConvexResults{
+            gitConvexApi(route: "${ROUTE_FETCH_REPO}"){
+              fetchRepo{
+                repoId
+                repoName
+                repoPath
+              }
+            }
+          }
+        `,
+      },
+    })
+      .then((res) => {
+        const apiResponse = res.data.data.gitConvexApi.fetchRepo;
+
+        if (apiResponse) {
+          const { repoId, repoName, repoPath } = apiResponse;
+          let repoContent = [];
+
+          repoId.forEach((entry, index) => {
+            repoContent.push({
+              id: entry,
+              repoName: repoName[index],
+              repoPath: repoPath[index],
+            });
+          });
+
+          setRepo(repoContent);
+        }
+      })
+      .catch((err) => {});
+
+    return () => {
+      source.cancel();
+    };
+  }, [props.searchQuery]);
+
+  return (
+    <div className="w-full">
+      {repo ? (
+        repo.map((item) => {
+          return (
+            <div className="my-4 p-4 border-b mx-auto flex justify-around items-center cursor-pointer hover:bg-gray-200">
+              <div className="w-1/2 text-2xl font-sand font-semibold">
+                {item.repoName}
+              </div>
+              <div className="w-1/4 block justify-center">
+                <div className="bg-blue-100 border shadow rounded p-2">
+                  PATH
+                </div>
+                <div className="my-2 text-lg font-light text-gray-700">
+                  {item.repoPath}
+                </div>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div className="text-center text-2xl font-sans font-light">
+          Loading...
+        </div>
+      )}
+    </div>
+  );
 }
