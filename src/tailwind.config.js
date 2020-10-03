9b38f1056d4fc4cf8c1531f8cb98699616eebe2d
Neel <neelusenthil1996@gmail.com>
2020-10-03 14:02:16 +0530

    added tailwind purge config to remove unwanted styles

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    purge: [
+      './src/**/*.html',
+      './src/**/*.jsx',
+      './src/**/*.js',
+    ],
+    theme: {},
+    variants: {},
+    plugins: [],
+  }
