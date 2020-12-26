c098709be2b14abcc9fec93424b1dea899fcf977
Neel <neelusenthil1996@gmail.com>
2020-12-26 12:40:31 +0530

    fix branch drop down expansion issue

--- src/Components/DashBoard/Repository/RepoComponents/RepositoryAction.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepositoryAction.js
@@ -27,7 +27,7 @@ export default function RepositoryAction() {
   });
   const [branchError, setBranchError] = useState(false);
   const [toggleSearchSelect, setToggleSearchSelect] = useState(false);
-  const [searchBranchValue, setSearchBranchValue] = useState('');
+  const [searchBranchValue, setSearchBranchValue] = useState("");
   const [filteredBranchList, setFilteredBranchList] = useState([]);
 
   const memoizedGitTracker = useMemo(() => {
@@ -162,7 +162,7 @@ export default function RepositoryAction() {
         setLoading(false);
         if (res.data.data && !res.data.error) {
           setActiveBranch(branchName);
-          setSearchBranchValue('');
+          setSearchBranchValue("");
           setFilteredBranchList([]);
           setToggleSearchSelect(!toggleSearchSelect);
         }
@@ -171,22 +171,28 @@ export default function RepositoryAction() {
         setLoading(false);
         if (err) {
           setBranchError(true);
-          event.target.innerText = activeBranch
+          event.target.innerText = activeBranch;
         }
       });
   }
 
   const searchBranchHandler = (e) => {
-    const searchBranch = e.target.value
-    setSearchBranchValue(searchBranch)
-    if(searchBranch !== '' && selectedRepoDetails && selectedRepoDetails.gitBranchList){
+    const searchBranch = e.target.value;
+    setSearchBranchValue(searchBranch);
+    if (
+      searchBranch !== "" &&
+      selectedRepoDetails &&
+      selectedRepoDetails.gitBranchList
+    ) {
       const { gitBranchList } = selectedRepoDetails;
-      const filteredBranches = gitBranchList.filter(branchName => branchName.toLowerCase().includes(searchBranch))
-      setFilteredBranchList(filteredBranches)
+      const filteredBranches = gitBranchList.filter((branchName) =>
+        branchName.toLowerCase().includes(searchBranch)
+      );
+      setFilteredBranchList(filteredBranches);
     } else {
-      setFilteredBranchList([])
+      setFilteredBranchList([]);
     }
-  }
+  };
 
   function activeRepoPane() {
     return (
@@ -235,24 +241,39 @@ export default function RepositoryAction() {
           <div className="flex items-center">
             <div className="flex-1 select--label mr-2">Branch</div>
             <div className="flex-1 flex flex-col justify-center">
-              <div className="flex-auto cursor-pointer inline-flex items-center justify-center px-4 py-2 shadow-md bg-indigo-50 border-indigo-400 text-indigo-700 border-dashed	border-b-2" onClick={(e) => setToggleSearchSelect(!toggleSearchSelect) }>
-                <span className='mr-2'>
-                {activeBranch}
-                </span>
-                <FontAwesomeIcon className='text-sm m-1' icon={["fas", "chevron-down"]}></FontAwesomeIcon>
+              <div
+                className="flex-auto cursor-pointer inline-flex items-center justify-center px-4 py-2 shadow-md bg-indigo-50 border-indigo-400 text-indigo-700 border-dashed	border-b-2"
+                onClick={(e) => {
+                  let target = e.currentTarget;
+                  if (!toggleSearchSelect) {
+                    target.style.width = "15rem";
+                  } else {
+                    target.style.width = "auto";
+                  }
+                  setToggleSearchSelect(!toggleSearchSelect);
+                }}
+              >
+                <span className="mr-2">{activeBranch}</span>
+                <FontAwesomeIcon
+                  className="text-sm m-1"
+                  icon={["fas", "chevron-down"]}
+                ></FontAwesomeIcon>
               </div>
-              {toggleSearchSelect ? <div className='flex-auto flex flex-row justify-center'>
-                <div className='flex flex-col bg-indigo-50 border-indigo-300 text-indigo-700 px-4 py-2 shadow-md rounded-md  z-20'>
-                <input
-                  id="branchSearchInput"
-                  type="text"
-                  placeholder="Search..."
-                  className="px-4 py-2 bg-indigo-100 mt-2 mb-2 text-indigo-700 shadow-sm rounded-sm focus:outline-none outline-none"
-                  onChange={searchBranchHandler}
-                  value={searchBranchValue}></input>
-                  {availableBranch()}
+              {toggleSearchSelect ? (
+                <div className="flex-auto flex flex-row justify-center">
+                  <div className="bg-indigo-50 border-indigo-300 text-indigo-700 px-4 py-2 shadow-md rounded-md z-20 absolute">
+                    <input
+                      id="branchSearchInput"
+                      type="text"
+                      placeholder="Search..."
+                      className="px-4 py-2 bg-indigo-100 mt-2 mb-2 text-indigo-700 shadow-sm rounded-sm focus:outline-none outline-none"
+                      onChange={searchBranchHandler}
+                      value={searchBranchValue}
+                    ></input>
+                    {availableBranch()}
+                  </div>
                 </div>
-              </div> : null}
+              ) : null}
             </div>
           </div>
         ) : null}
@@ -275,33 +296,43 @@ export default function RepositoryAction() {
 
   const branchCardComponent = (branch) => {
     return (
-      <div key={branch} value={branch} className="cursor-pointer text-sm shadow-sm p-1 mt-1 mb-1" onClick={(event) => {
-        event.persist();
-        setActiveBranch("...");
-        setTrackingBranch(event.target.innerText, event);}}>
+      <div
+        key={branch}
+        value={branch}
+        className="cursor-pointer text-sm shadow-sm p-1 mt-1 mb-1"
+        onClick={(event) => {
+          event.persist();
+          setActiveBranch("...");
+          setTrackingBranch(event.target.innerText, event);
+        }}
+      >
         {branch}
       </div>
     );
-  }
+  };
 
   function availableBranch() {
     if (selectedRepoDetails && selectedRepoDetails.gitBranchList) {
       const { gitBranchList } = selectedRepoDetails;
-      if(searchBranchValue !== ''){
-        if(filteredBranchList.length > 0){
+      if (searchBranchValue !== "") {
+        if (filteredBranchList.length > 0) {
           return filteredBranchList.map((branch, index) => {
             if (branch !== "NO_BRANCH") {
-              return branchCardComponent(branch)
+              return branchCardComponent(branch);
             }
             return null;
           });
-        } else{
-          return (<div className='text-center font-sans font-light text-xl my-2 text-gray-600 border-b border-dotted'>{searchBranchValue.concat(' branch not available!')}</div>)
+        } else {
+          return (
+            <div className="text-center font-sans font-light text-xl my-2 text-gray-600 border-b border-dotted">
+              {searchBranchValue.concat(" branch not available!")}
+            </div>
+          );
         }
       } else {
         return gitBranchList.map((branch, index) => {
           if (branch !== "NO_BRANCH") {
-            return branchCardComponent(branch)
+            return branchCardComponent(branch);
           }
           return null;
         });
