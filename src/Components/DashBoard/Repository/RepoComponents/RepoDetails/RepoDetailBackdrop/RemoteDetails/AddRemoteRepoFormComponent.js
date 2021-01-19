4847f5616578c139fa94bb3f8fe143d687010e3d
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2021-01-19 17:03:32 +0530

    Error handler added for duplicate remote name

--- src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/RemoteDetails/AddRemoteRepoFormComponent.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/RemoteDetails/AddRemoteRepoFormComponent.js
@@ -18,6 +18,7 @@ export default function AddRemoteRepoFormComponent(props) {
         ref={formId === "remoteName" ? remoteNameRef : remoteUrlRef}
         onChange={() => {
           props.setFieldMissing(false);
+          props.setAddRemoteStatus(false);
         }}
       ></input>
     );
@@ -27,9 +28,9 @@ export default function AddRemoteRepoFormComponent(props) {
     let remoteName = remoteNameRef.current.value;
     let remoteUrl = remoteUrlRef.current.value;
     if (remoteName && remoteUrl) {
+      props.remoteDetail({ remoteName: remoteName, remoteUrl: remoteUrl });
       remoteNameRef.current.value = "";
       remoteUrlRef.current.value = "";
-      props.remoteDetail({ remoteName: remoteName, remoteUrl: remoteUrl });
     } else {
       props.remoteDetail("");
     }
@@ -37,15 +38,15 @@ export default function AddRemoteRepoFormComponent(props) {
 
   return (
     <form className="form--data flex w-full items-center my-6">
-      <div className="mx-auto" style={{ width: "25%" }}>
+      <div className="mx-auto w-1/4">
         {formAddRemote("remoteName", "Remote name")}
       </div>
-      <div className="mx-auto" style={{ width: "50%" }}>
+      <div className="mx-auto w-1/2">
         {formAddRemote("remoteURL", "Remote URL")}
       </div>
       <div
         className="text-center flex items-center justify-evenly"
-        style={{ outline: "none", width: "20%" }}
+        style={{ outline: "none", width: "22%" }}
       >
         <div
           className="text-lg items-center p-1 rounded w-1/3 mx-auto cursor-pointer bg-blue-500 hover:bg-blue-700 font-semibold"
@@ -59,11 +60,12 @@ export default function AddRemoteRepoFormComponent(props) {
           ></FontAwesomeIcon>
         </div>
         <div
-          className="text-lg items-center p-1 rounded w-1/3 mx-auto cursor-pointer bg-red-400 hover:bg-red-600 font-semibold"
+          className="text-lg items-center p-1 rounded w-1/3 mx-auto cursor-pointer bg-red-500 hover:bg-red-600 font-semibold"
           onClick={() => {
             props.setAddNewRemote(true);
             props.setRemoteForm(false);
             props.setFieldMissing(false);
+            props.setAddRemoteStatus(false);
           }}
         >
           <FontAwesomeIcon
