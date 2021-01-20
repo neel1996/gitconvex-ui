25b954a67a47e4e35ebf902439c73ce697cd35ac
Nageswari SV <65342122+Nageswari-droid@users.noreply.github.com>
2021-01-21 00:47:16 +0530

    Completed edit operation with success and failure states

--- src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/RemoteDetails/RemoteCard.js
+++ src/Components/DashBoard/Repository/RepoComponents/RepoDetails/RepoDetailBackdrop/RemoteDetails/RemoteCard.js
@@ -27,9 +27,17 @@ export default function RemoteCard({
 
   const [remoteNameState, setRemoteNameState] = useState(remoteName);
   const [remoteUrlState, setRemoteUrlState] = useState(remoteUrl);
-
   const [editRemote, setEditRemote] = useState(false);
+  let [editNameState, setEditNameState] = useState(false);
+
   const changeState = (name, url) => {
+    remoteDetails.forEach((items) => {
+      if (items.remoteName === remoteNameState) {
+        items.remoteName = name;
+        items.remoteUrl = url;
+      }
+    });
+    setRemoteDetails(remoteDetails);
     setRemoteNameState(name);
     setRemoteUrlState(url);
     setEditRemote(false);
@@ -116,27 +124,34 @@ export default function RemoteCard({
             <div
               className="text-lg items-center p-1 py-2 rounded w-1/4 mx-auto cursor-pointer bg-blue-500 hover:bg-blue-700 font-semibold"
               onClick={() => {
-                let name = remoteFormName.current.value;
-                let url = remoteFormUrl.current.value;
-                if (name && url) {
+                let name;
+                let url = !remoteFormUrl.current.value
+                  ? remoteUrlState
+                  : remoteFormUrl.current.value;
+                if (
+                  !remoteFormName.current.value ||
+                  remoteFormName.current.value === remoteNameState
+                ) {
+                  name = remoteNameState;
+                  editNameState = false;
+                  setEditNameState(false);
+                } else {
+                  name = remoteFormName.current.value;
+                  editNameState = true;
+                  setEditNameState(true);
+                }
+                if (editNameState) {
                   if (
                     !remoteDetails.find((items) => {
                       return items.remoteName === name;
                     })
                   ) {
-                    remoteDetails.forEach((items) => {
-                      if (items.remoteName === remoteNameState) {
-                        items.remoteName = name;
-                        items.remoteUrl = url;
-                      }
-                    });
-                    setRemoteDetails(remoteDetails);
                     changeState(name, url);
                   } else {
                     setAddRemoteStatus(true);
                   }
                 } else {
-                  setFieldMissing(true);
+                  changeState(name, url);
                 }
               }}
             >
